b678fb440955bfbe655325d4999f5f9d7af1803d
Nicholsd Engleman <nicholas@englemaninvest.com>
2019-08-26 22:30:33 -0400

    Fine-tune positioning of tooltips for large data sets

--- src/data.js
+++ src/data.js
@@ -98,6 +98,631 @@ export const data = [
         id: 931222,
         value: 10,
         tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: -5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 10,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 1,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 2,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 3,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 4,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 5,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 6,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 7,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 8,
+        tooltip: ["Bitcoin", "453%"]
+    },
+    {
+        id: 931222,
+        value: 9,
+        tooltip: ["Bitcoin", "453%"]
     }
 
 ];
